09df37c3c1af35f763d2e5615a27490cf2afc5bf
Dino <dino@tnv.edu.vn>
2020-05-06 09:13:57 +0700

    Initial Commit

---
+++ Demo Notification/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Demo Notification
+//
+//  Created by Softone on 5/6/20.
+//  Copyright © 2020 Softone. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
